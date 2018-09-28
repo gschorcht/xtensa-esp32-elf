554e3df093ec1ac8b3786b3358b4757b901ac754
Schorcht <gunar@schorcht.net>
2018-09-28 15:40:47 +0200

    initial commit

---
+++ lib/gcc/xtensa-esp32-elf/5.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/gs/esp/crosstool-NG/.build/src/gcc-5.2.0/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=xtensa-esp32-elf --prefix=/home/gs/esp/crosstool-NG/builds/xtensa-esp32-elf --with-local-prefix=/home/gs/esp/crosstool-NG/builds/xtensa-esp32-elf/xtensa-esp32-elf/sysroot --with-sysroot=/home/gs/esp/crosstool-NG/builds/xtensa-esp32-elf/xtensa-esp32-elf/sysroot --with-newlib --enable-threads=no --disable-shared --with-pkgversion='crosstool-NG crosstool-ng-1.22.0-80-g6c4433a' --disable-__cxa_atexit --enable-cxx-flags='-fno-rtti -ffunction-sections' --with-gmp=/home/gs/esp/crosstool-NG/.build/xtensa-esp32-elf/buildtools --with-mpfr=/home/gs/esp/crosstool-NG/.build/xtensa-esp32-elf/buildtools --with-mpc=/home/gs/esp/crosstool-NG/.build/xtensa-esp32-elf/buildtools --with-isl=/home/gs/esp/crosstool-NG/.build/xtensa-esp32-elf/buildtools --with-cloog=/home/gs/esp/crosstool-NG/.build/xtensa-esp32-elf/buildtools --with-libelf=/home/gs/esp/crosstool-NG/.build/xtensa-esp32-elf/buildtools --enable-lto --enable-target-optspace --without-long-double-128 --disable-libgomp --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-nls --enable-languages=c,c++ --disable-libstdcxx-verbose --enable-threads=posix --enable-gcov-custom-rtio";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
